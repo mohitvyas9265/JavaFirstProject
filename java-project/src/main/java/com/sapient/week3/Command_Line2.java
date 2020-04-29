ab40dc48c09c880740c9b56984e319f761825453
mohitvyas9265 <mohitvyas9265@gmail.com>
2020-04-30 01:09:31 +0545

    Created a java class to read n numbers from key board and display , sort and  search on the array

---
+++ java-project/src/main/java/com/sapient/week3/Command_Line2.java
@@ -0,0 +1,44 @@
+package com.sapient.week3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class command_line2 
+{
+	public void read()
+	{
+		Scanner sc=new Scanner(System.in);
+		int n = Integer.ma
+		 int arr[]=new int[n];
+		 int temp;
+		for(int i=0;i<n;i++)
+		{
+			temp=sc.nextInt();
+			arr[i]=temp;
+		}
+		sc.close();
+	}
+	public void display(int [] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	public void sort(int [] arr)
+	{
+		Arrays.sort(arr);
+	}
+	public boolean find(int [] arr,int n)
+	{
+		 sort(arr);
+		 int a =  Arrays.binarySearch(arr, n);
+			if(a > 0)
+				return true;
+			else
+				return false;
+
+	}
+	
+
+};
